4c00fb6745fd3c3eaf6491349223ce221f5e859c
BasLee <bas.leenknegt@meertens.knaw.nl>
2018-04-18 15:17:28 +0200

    Remove interactive and tty argument of command to run integration tests

--- start-vre.sh
+++ start-vre.sh
@@ -25,4 +25,4 @@ if [ "$CONFIGURE_OWNCLOUD" = true ]; then
 fi
 
 echo "Run integration tests (add 'debug' to start in debug mode)"
-docker exec -it vre_integration_1 /usr/local/integration/docker-run-integration-tests.sh $1
+docker exec vre_integration_1 /usr/local/integration/docker-run-integration-tests.sh $1
