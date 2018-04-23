7d7d6002a9374090cd29487851373221d70ac46a
Vic Ding <qiqing.ding@meertens.knaw.nl>
2018-04-23 17:44:43 +0200

    fixing issues

--- integration/src/test/java/nl/knaw/meertens/clariah/vre/integration/DeployServiceTest.java
+++ integration/src/test/java/nl/knaw/meertens/clariah/vre/integration/DeployServiceTest.java
@@ -47,7 +47,7 @@ public class DeployServiceTest extends AbstractIntegrationTest {
 
         String workDir = startDeploymentWithInputFileId(inputFileId);
 
-        TimeUnit.SECONDS.sleep(1);
+        TimeUnit.SECONDS.sleep(3);
 
         checkStatusIsRunning(workDir);
 
