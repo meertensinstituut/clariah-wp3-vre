00a9072438421fe16d87614edee68e489954ec18
BasLee <bas.leenknegt@meertens.knaw.nl>
2018-05-24 13:15:43 +0200

    Add jquey (problem with 'Access-Control-Allow-Origin')

---
+++ ui/docker-run-ui.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Variables should start with 'REACT_APP' for them to be passed to react app:
+REACT_APP_KEY_GET_OBJECTS="${APP_KEY_GET_OBJECTS}"
+
+npm start
--- ui/src/app/app.js
+++ ui/src/app/app.js
@@ -1,14 +1,28 @@
 import React from "react";
+import $ from "jquery";
 
 export default class App extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            msg: "Welcome."
+            msg: "Welcome..."
         };
+        this.apiKey = process.env.REACT_APP_KEY_GET_OBJECTS;
     }
 
+    getObject(id) {
+        $.get({
+            url: "http://localhost:8089/api/v2/objects/_table/object?limit=1",
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('X-DreamFactory-Api-Key', this.apiKey)
+            },
+        }).then("hello success?");
+    }
+
+
     render() {
+        this.getObject(1);
         return (
             <div className="main">
                 <p>{this.state.msg}</p>
