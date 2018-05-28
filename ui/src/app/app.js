c883da210d944613f4dcec2d678e23da63d3b380
BasLee <bas.leenknegt@meertens.knaw.nl>
2018-05-28 10:05:40 +0200

    Add some minimal styling

--- ui/src/app/app.js
+++ ui/src/app/app.js
@@ -1,20 +1,41 @@
 import React from "react";
 import Objects from "./objects";
+import {Alert, Col, Grid, Row, Tab, Tabs} from "react-bootstrap";
 
 export default class App extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            title: "VRE",
+            subtitle: "A CLARIAH WP3 Virtual Research Environment",
             msg: "Welcome to Clariah's VRE",
         };
     }
 
     render() {
         return (
-            <div className="main">
-                <p>{this.state.msg}</p>
-                <Objects/>
+            <div>
+                <Grid>
+                    <Row>
+                        <Col xs={12} md={12} className="header">
+                            <h1>{this.state.title}</h1>
+                            <p>{this.state.subtitle}</p>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col xs={12} md={12} className="header">
+                            <Tabs defaultActiveKey={1} id="primary-tabs">
+                                <Tab eventKey={1} title="Home">
+                                    <Alert bsStyle="info">{this.state.msg}</Alert>
+                                </Tab>
+                                <Tab eventKey={2} title="Objects">
+                                    <Objects/>
+                                </Tab>
+                            </Tabs>
+                        </Col>
+                    </Row>
+                </Grid>
             </div>
         );
     }
