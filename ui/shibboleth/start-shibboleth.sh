1006c4b3bd166b626c6ced38d29969d62ec45fab
BasLee <bas.leenknegt@meertens.knaw.nl>
2018-08-27 14:47:07 +0200

    [UI] Add apache server, to put UI behind a shibboleth proxy

---
+++ ui/shibboleth/start-shibboleth.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker-compose -p shibboleth up -d
--- ui/start-ui.sh
+++ ui/start-ui.sh
@@ -3,7 +3,7 @@
 # use environment variables from root:
 cp ../.env ./.env
 
-# Variables should start with 'REACT_APP' for them to be passed to react app:
+# variables that start with 'REACT_APP' are passed to react app:
 REACT_APP_KEY_GET_OBJECTS="${APP_KEY_GET_OBJECTS}"
 
 echo "start UI..."
