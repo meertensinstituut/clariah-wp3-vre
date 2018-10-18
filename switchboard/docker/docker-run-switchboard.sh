306827dfe78e9b6cf06516c0f891cd9f67535040
BasLee <bas.leenknegt@meertens.knaw.nl>
2018-10-18 10:02:43 +0200

    [Switchboard] Upgrade java version to 11

--- switchboard/docker/docker-run-switchboard.sh
+++ switchboard/docker/docker-run-switchboard.sh
@@ -13,7 +13,7 @@ echo "deploy switchboard.war..." && \
 cp /usr/local/switchboard/target/switchboard-1.0-SNAPSHOT.war /usr/local/tomcat/webapps/switchboard.war && \
 
 echo "start tomcat..." && \
-export CATALINA_OPTS="-Xmx700m --add-modules java.xml.bind,java.xml.ws --add-exports java.base/jdk.internal.ref=ALL-UNNAMED" && \
+export CATALINA_OPTS="-Xmx700m --add-exports java.base/jdk.internal.ref=ALL-UNNAMED" && \
 /usr/local/tomcat/bin/catalina.sh run
 
 # keep docker running when redeploying jar:
