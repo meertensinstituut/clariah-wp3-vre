aef0937ee7f697588f48522059e0570563c5d9c3
BasLee <bas.leenknegt@meertens.knaw.nl>
2018-12-10 09:46:57 +0100

    [Switchboard] Validate cmdi using schemanon

--- switchboard/src/main/java/nl/knaw/meertens/clariah/vre/switchboard/registry/services/ServicesRegistryServiceImpl.java
+++ switchboard/src/main/java/nl/knaw/meertens/clariah/vre/switchboard/registry/services/ServicesRegistryServiceImpl.java
@@ -5,13 +5,21 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import nl.mpi.tla.schemanon.SchemAnon;
+import nl.mpi.tla.schemanon.SchemAnonException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static java.lang.String.format;
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toList;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
 public class ServicesRegistryServiceImpl implements ServicesRegistryService {
@@ -21,6 +29,7 @@ public class ServicesRegistryServiceImpl implements ServicesRegistryService {
   private final String serviceDbUrl;
   private final String serviceDbKey;
   private final ObjectMapper mapper;
+  private final SchemAnon tron;
 
   public ServicesRegistryServiceImpl(String serviceDbUrl, String serviceDbKey, ObjectMapper mapper) {
     if (isBlank(serviceDbUrl)) {
@@ -32,6 +41,7 @@ public class ServicesRegistryServiceImpl implements ServicesRegistryService {
     }
     this.serviceDbKey = serviceDbKey;
     this.mapper = mapper;
+    this.tron = new SchemAnon(requireNonNull(getClass().getClassLoader().getResource("valid-service-cmd.sch")));
   }
 
   @Override
@@ -39,7 +49,7 @@ public class ServicesRegistryServiceImpl implements ServicesRegistryService {
     String body = "";
     try {
       String service = "_table/service";
-      String url = String.format(
+      String url = format(
         "%s/%s/%d",
         serviceDbUrl,
         service,
@@ -49,7 +59,7 @@ public class ServicesRegistryServiceImpl implements ServicesRegistryService {
       body = response.getBody();
       return mapper.readValue(body, ServiceRecord.class);
     } catch (UnirestException | IOException | IllegalStateException e) {
-      throw new RuntimeException(String.format("Could not get service for id [%s]; response: [%s]", id, body), e);
+      throw new RuntimeException(format("Could not get service for id [%s]; response: [%s]", id, body), e);
     }
   }
 
@@ -62,7 +72,7 @@ public class ServicesRegistryServiceImpl implements ServicesRegistryService {
   @Override
   public ServiceRecord getServiceByName(String name) {
     var service = "_table/service";
-    var url = String.format(
+    var url = format(
       "%s/%s/?filter=name%%20like%%20%s",
       serviceDbUrl,
       service,
@@ -70,34 +80,65 @@ public class ServicesRegistryServiceImpl implements ServicesRegistryService {
     );
     var services = this.getServices(url);
     if (services.isEmpty()) {
-      throw new IllegalStateException(String.format("No services found for service [%s]", name));
+      throw new IllegalStateException(format("No services found for service [%s]", name));
     }
     return services.get(0);
   }
 
-  @Override
-  public List<ServiceRecord> getServicesByMimetype(String mimetype) {
-    var serviceWithMimetypeView = "_table/service_with_mimetype";
-    var url = String.format(
-      "%s/%s?filter=mimetype%%20%%3D%%20%s",
-      serviceDbUrl,
-      serviceWithMimetypeView,
-      mimetype
-    );
-    return getServices(url);
-  }
 
   @Override
   public List<ServiceRecord> getServicesByMimetypeAndKind(String mimetype, ServiceKind kind) {
     var serviceWithMimetypeView = "_table/service_with_mimetype";
-    var url = String.format(
+
+    var url = format(
       "%s/%s?filter=(mimetype%%20%%3D%%20%s)%%20and%%20(kind%%20like%%20%s)",
       serviceDbUrl,
       serviceWithMimetypeView,
       mimetype,
       kind.getKind()
     );
-    return getServices(url);
+    return getServices(url)
+      .stream()
+      .filter(this::isValidCmdi)
+      .collect(toList());
+  }
+
+  private boolean isValidCmdi(ServiceRecord serviceRecord) {
+    try {
+      var tmpFile = File.createTempFile(serviceRecord.getName(), "cmdi");
+      var out = new BufferedWriter(new FileWriter(tmpFile));
+      out.write(serviceRecord.getSemantics());
+      out.close();
+      return isValidCmdi(tmpFile);
+    } catch (IOException ex) {
+      throw new RuntimeException("Could not validate cmdi", ex);
+    }
+  }
+
+  private boolean isValidCmdi(File input) {
+    try {
+      if (tron.validate(input)) {
+        return true;
+      } else {
+        for (var msg : tron.getMessages()) {
+          var location = !isBlank(msg.getLocation()) ? format("at %s", msg.getLocation()) : "";
+          var notValidMsg = format("Semantics not valid %s:", location);
+          if (msg.isError()) {
+            logger.error(notValidMsg);
+            logger.error(msg.getText());
+          } else {
+            logger.warn(notValidMsg);
+            logger.warn(msg.getText());
+          }
+        }
+        return false;
+      }
+    } catch (SchemAnonException | IOException ex) {
+      throw new RuntimeException(format(
+        "Could not validate CMDI for service [%s]",
+        input.toPath().toString()
+      ), ex);
+    }
   }
 
   private List<ServiceRecord> getServices(String url) {
@@ -107,7 +148,7 @@ public class ServicesRegistryServiceImpl implements ServicesRegistryService {
       var reader = mapper.readerFor(new TypeReference<List<ServiceRecord>>() {
       });
       List<ServiceRecord> serviceRecords = reader.readValue(resource);
-      logger.info(String.format(
+      logger.info(format(
         "Url [%s] yielded services [%s]",
         url,
         serviceRecords
@@ -117,7 +158,7 @@ public class ServicesRegistryServiceImpl implements ServicesRegistryService {
       ));
       return serviceRecords;
     } catch (UnirestException | IOException e) {
-      throw new RuntimeException(String.format("Could not determine services for url [%s]", url), e);
+      throw new RuntimeException(format("Could not determine services for url [%s]", url), e);
     }
   }
 
@@ -129,7 +170,7 @@ public class ServicesRegistryServiceImpl implements ServicesRegistryService {
       .header("X-DreamFactory-Api-Key", serviceDbKey)
       .asString();
     if (response.getStatus() != 200) {
-      throw new IllegalStateException(String.format(
+      throw new IllegalStateException(format(
         "Services registry response status was not 200 but [%d]",
         response.getStatus()
       ));
