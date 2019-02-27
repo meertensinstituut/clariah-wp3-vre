16f7a11abce39693a701979660c0d1d777e7162b
BasLee <bas.leenknegt@meertens.knaw.nl>
2019-02-27 11:27:34 +0100

    [Tagger] Make sure all env vars are mocked in surefire plugin

--- tagger/src/main/java/nl/knaw/meertens/clariah/vre/tagger/Config.java
+++ tagger/src/main/java/nl/knaw/meertens/clariah/vre/tagger/Config.java
@@ -9,16 +9,20 @@ import static nl.knaw.meertens.clariah.vre.tagger.FileAction.RENAME;
 import static nl.knaw.meertens.clariah.vre.tagger.FileAction.UPDATE;
 
 public class Config {
+
+  // Environment variables:
   public static final String KAFKA_SERVER = "kafka:" + requireNonNull(System.getenv("KAFKA_PORT"));
   public static final String RECOGNIZER_TOPIC_NAME = requireNonNull(System.getenv("RECOGNIZER_TOPIC_NAME"));
   public static final String RECOGNIZER_GROUP_NAME = requireNonNull(System.getenv("RECOGNIZER_GROUP_NAME"));
   public static final String TAGGER_TOPIC_NAME = requireNonNull(System.getenv("TAGGER_TOPIC_NAME"));
+  public static final String OBJECTS_DB_KEY = requireNonNull(System.getenv("APP_KEY_OBJECTS"));
+
+  // TODO: use shibboleth:
+  public static final String TEST_USER = requireNonNull(requireNonNull(System.getenv("TEST_USER")));
+
   public static final String OBJECTS_DB_URL = "http://dreamfactory/api/v2/objects";
-  public static final String OBJECTS_DB_KEY = System.getenv("APP_KEY_OBJECTS");
   public static final String OBJECT_TABLE = "/_table/object";
   public static final List<String> ACTIONS_TO_TAG =
     newArrayList(CREATE.msgValue(), UPDATE.msgValue(), RENAME.msgValue());
   public static final String SYSTEM_TAG_OWNER = "system";
-  // TODO: use shibboleth:
-  public static final String TEST_USER = requireNonNull(System.getenv("TEST_USER"));
 }
