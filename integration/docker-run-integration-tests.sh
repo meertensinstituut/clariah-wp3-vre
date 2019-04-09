e012b100163710154b67e0f3aac247143d0168f1
Vic Ding <qiqing.ding@meertens.knaw.nl>
2019-04-09 15:44:55 +0200

    [integration] run full test again

--- integration/docker-run-integration-tests.sh
+++ integration/docker-run-integration-tests.sh
@@ -5,9 +5,9 @@ echo 'start integration tests...'
 if [ "$1" = "debug" ]; then
   # Change maven remote debug port to '*:8085':
   sed -i -e 's/8000/*:8085/g' /usr/share/maven/bin/mvnDebug
-  mvnDebug -DfailIfNoTests=false -Dtest=FoliaEditorTest test
-#  mvnDebug test
+#  mvnDebug -DfailIfNoTests=false -Dtest=FoliaEditorTest test
+  mvnDebug test
 else
-  mvn clean -DfailIfNoTests=false -Dtest=FoliaEditorTest test
-#  mvn clean test
+#  mvn clean -DfailIfNoTests=false -Dtest=FoliaEditorTest test
+  mvn clean test
 fi
