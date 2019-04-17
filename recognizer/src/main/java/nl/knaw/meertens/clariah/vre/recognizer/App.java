c5aa6a18ab455199271d20d0af84e99697aba37c
BasLee <bas.leenknegt@meertens.knaw.nl>
2019-04-17 11:43:09 +0200

    [Recognizer] Add recognizer.log to logback config. Clean up logging

--- recognizer/src/main/java/nl/knaw/meertens/clariah/vre/recognizer/App.java
+++ recognizer/src/main/java/nl/knaw/meertens/clariah/vre/recognizer/App.java
@@ -5,23 +5,11 @@ import org.slf4j.LoggerFactory;
 
 public class App {
 
-  private final static Logger logger = LoggerFactory.getLogger(App.class.getName());
+  private static final Logger logger = LoggerFactory.getLogger(App.class);
 
   private static final RecognizerService recognizerService = new RecognizerService();
 
   public static void main(String[] args) {
-
-    var message = "logtest5";
-    logger.trace(message);
-    logger.debug(message);
-    logger.info(message);
-    logger.warn(message);
-    logger.error(message);
-
-    startConsuming();
-  }
-
-  private static void startConsuming() {
     logger.info("Start consuming nextcloud topic...");
     recognizerService.consumeOwncloud();
   }
