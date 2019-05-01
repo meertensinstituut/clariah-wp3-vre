e4d3966c7f772fbc50778d500c174c629a4dd682
BasLee <bas.leenknegt@meertens.knaw.nl>
2019-05-01 14:20:34 +0200

    [Deployment] Use parent pom

---
+++ deployment-service/build-with-local-parent-pom.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+mkdir ./parent-pom/
+cp ../parent-pom/pom.xml ./parent-pom/
+
+mv Dockerfile Dockerfile.original
+mv Dockerfile.localParentPom Dockerfile
+
+docker-compose build deployment
+
+mv Dockerfile Dockerfile.localParentPom
+mv Dockerfile.original Dockerfile
+
+rm -rf ./parent-pom/
