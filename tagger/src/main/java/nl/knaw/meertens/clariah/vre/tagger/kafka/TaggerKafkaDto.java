b843a7915a436b426a9720feec44f9d18928ae5f
BasLee <bas.leenknegt@meertens.knaw.nl>
2019-05-01 13:35:40 +0200

    [Tagger] Use parent pom

--- tagger/src/main/java/nl/knaw/meertens/clariah/vre/tagger/kafka/TaggerKafkaDto.java
+++ tagger/src/main/java/nl/knaw/meertens/clariah/vre/tagger/kafka/TaggerKafkaDto.java
@@ -1,11 +1,12 @@
 package nl.knaw.meertens.clariah.vre.tagger.kafka;
 
-import org.joda.time.LocalDateTime;
+import static java.time.LocalDateTime.now;
+import static java.time.format.DateTimeFormatter.ofPattern;
 
 public class TaggerKafkaDto {
   public String msg;
   public String owner;
   public Long object;
   public Long tag;
-  public String dateTime = LocalDateTime.now().toString("yyyy-MM-dd HH:mm");
+  public String dateTime = ofPattern("yyyy-MM-dd HH:mm").format(now());
 }
