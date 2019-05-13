7b7e76d1cd11b1d251beeb5ec1426ad12e706b7b
BasLee <bas.leenknegt@meertens.knaw.nl>
2019-05-13 15:29:33 +0200

    [Recognizer] Make plugin adhere to checkstyle rules

--- recognizer/src/main/java/nl/knaw/meertens/clariah/vre/recognizer/semantics/FoLiA.java
+++ recognizer/src/main/java/nl/knaw/meertens/clariah/vre/recognizer/semantics/FoLiA.java
@@ -1,51 +1,49 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.knaw.meertens.clariah.vre.recognizer.semantics;
 
+import net.sf.saxon.s9api.SaxonApiException;
+import nl.mpi.tla.util.Saxon;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.transform.stream.StreamSource;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.xml.transform.stream.StreamSource;
-import net.sf.saxon.s9api.SaxonApiException;
-import net.sf.saxon.s9api.XdmNode;
-import nl.mpi.tla.util.Saxon;
 
-/**
- *
- * @author menzowi
- */
+import static nl.mpi.tla.util.Saxon.xpath2boolean;
+
 public class FoLiA implements SemanticTypePlugin {
-    
-    private static final Map<String,String> NAMESPACES = new HashMap<>();
-    
-    static {
-        NAMESPACES.put("folia","http://ilk.uvt.nl/folia");
+
+  private static final Map<String, String> NAMESPACES = new HashMap<>();
+
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  static {
+    NAMESPACES.put("folia", "http://ilk.uvt.nl/folia");
+  }
+
+  @Override
+  public List<String> detect(Path object) {
+    var types = new ArrayList<String>();
+    try {
+      var folia = Saxon.buildDocument(new StreamSource(object.toFile()));
+      if (xpath2boolean(folia, "exists(/folia:FoLiA/folia:metadata/folia:annotations/folia:token-annotation)", null,
+        NAMESPACES)) {
+        types.add("folia.token");
+      }
+      if (xpath2boolean(folia,
+        "/folia:FoLiA/folia:metadata/folia:annotations/folia:pos-annotation/@set='https://raw.githubusercontent" +
+          ".com/proycon/folia/master/setdefinitions/frog-mbpos-cgn'",
+        null, NAMESPACES)) {
+        types.add("folia.pos");
+        types.add("folia.pos.cgn");
+      }
+    } catch (SaxonApiException ex) {
+      logger.error("Could not detect folia semantics", ex);
     }
-    
-    
-    @Override
-    public List<String> detect(Path object) {
-        List<String> types = new ArrayList<>();
-        try {
-            XdmNode folia = Saxon.buildDocument(new StreamSource(object.toFile()));
-            if (Saxon.xpath2boolean(folia, "exists(/folia:FoLiA/folia:metadata/folia:annotations/folia:token-annotation)", null, NAMESPACES)) {
-                types.add("folia.token");
-            }
-            if (Saxon.xpath2boolean(folia, "/folia:FoLiA/folia:metadata/folia:annotations/folia:pos-annotation/@set='https://raw.githubusercontent.com/proycon/folia/master/setdefinitions/frog-mbpos-cgn'", null, NAMESPACES)) {
-                types.add("folia.pos");
-                types.add("folia.pos.cgn");
-            }
-        } catch (SaxonApiException ex) {
-            Logger.getLogger(FoLiA.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return types;
-    }
-    
+    return types;
+  }
+
 }
--- recognizer/src/main/java/nl/knaw/meertens/clariah/vre/recognizer/semantics/SemanticTypePlugin.java
+++ recognizer/src/main/java/nl/knaw/meertens/clariah/vre/recognizer/semantics/SemanticTypePlugin.java
@@ -1,19 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package nl.knaw.meertens.clariah.vre.recognizer.semantics;
 
 import java.nio.file.Path;
 import java.util.List;
 
-/**
- *
- * @author menzowi
- */
 public interface SemanticTypePlugin {
-    
-    List<String> detect(Path object);
-    
+
+  List<String> detect(Path object);
+
 }
